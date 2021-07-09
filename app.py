f77fe5e2ce7c7ff07f0ed67fb20e4a38898b536c
mxzpytlk <mxzpytlk@gmail.com>
2021-07-09 20:48:17 +0300

    Delete cors

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///luca.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-CORS(app)
 
 db = SQLAlchemy(app)
 
